a89a32b4196a2b7bb804b074d927b448d8945180
Fauricio Rojas <fauricio.rojas@hpe.com>
2016-10-22 16:40:43 -0600

    Fixed bug in module4-solution's script.js

--- module4-solution/js/script.js
+++ module4-solution/js/script.js
@@ -2,7 +2,7 @@
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
   for (var index in names) {
-    var firstLetter = names[i].charAt(0).toLowerCase();
+    var firstLetter = names[index].charAt(0).toLowerCase();
 
     if (firstLetter === 'j') {
       byeSpeaker.speak(names[index]);
