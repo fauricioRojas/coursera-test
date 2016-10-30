39be8c1e50de1a40fbadafa3a3218883a6004bf5
Fauricio Rojas <fauricio.rojas@hpe.com>
2016-10-30 16:32:34 -0600

    Enhancement in module1-solution-angularjs/js/app.js

--- module1-solution-angularjs/js/app.js
+++ module1-solution-angularjs/js/app.js
@@ -14,7 +14,9 @@
 
     $scope.checkDishes = function () {
       if ($scope.dishes.length) {
-        var itemsList = $scope.dishes.split(',');
+        var dishes = $scope.dishes.replace(/\s/g, ''); // Remove all empty spaces before split.
+        var itemsList = dishes.split(','); // Split by comma.
+        
         itemsList = itemsList.filter (function (element) {
           return element !== '';
         });
@@ -26,20 +28,12 @@
           $scope.message = 'Too much!';
         }
 
-        $scope.messageColor = {
-          'color': 'green'
-        };
-        $scope.inputBorderColor = {
-          'border-color': 'green'
-        };
+        $scope.messageColor = 'color: green';
+        $scope.inputBorderColor = 'border-color: green';
       } else {
         $scope.message = 'Please enter data first';
-        $scope.messageColor = {
-          'color': 'red'
-        };
-        $scope.inputBorderColor = {
-          'border-color': 'red'
-        };
+        $scope.messageColor = 'color: red';
+        $scope.inputBorderColor = 'border-color: red';
       }
     };
   }
